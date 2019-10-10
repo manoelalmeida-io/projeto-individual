536a3910196fd69a867203490951cdb9a9282f27
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-10-10 18:53:59 -0300

    Adicionando imagens dos pokémons

---
+++ js/data.js
@@ -0,0 +1,15 @@
+var data = {
+    pokemons = [
+        {
+            "id": 1,
+            "nome": "Bulbasaur",
+            "tipo": [ "Grama", "Veneno" ],
+            "hp": 45,
+            "ataque": 49,
+            "defesa": 49,
+            "ataqueEsp": 65,
+            "defesaEsp": 65,
+            "velocidade": 45
+        }
+    ]
+}
--- js/time.js
+++ js/time.js
@@ -1,8 +1,41 @@
+var pokemons = document.getElementsByClassName("pokemon");
+var contador = 0;
+
+while (contador < 150) {
+
+    availablePokemons.innerHTML += `<div class="pokemon" onclick="selecionar(this)">${fixarCasas(++contador)}</div>`;
+}
+
+contador = 0;
+
+while (contador < pokemons.length) {
+    
+    pokemons[contador++].style.backgroundImage = `url("img/pokemons/${fixarCasas(contador)}.png")`;
+}
+
 function selecionar(e) {
     
     if (selectedPokemons.childElementCount < 6) {
         
         var clone = e.cloneNode(true);
         selectedPokemons.appendChild(clone);
+    }
+}
+
+function fixarCasas(numero) {
+
+    if (numero < 10) {
+        
+        return "00" + numero;
+    }
+
+    if (numero >= 10 && numero < 100) {
+
+        return "0" + numero;
+    }
+
+    if (numero >= 100) {
+        
+        return "" + numero;
     }
 }
