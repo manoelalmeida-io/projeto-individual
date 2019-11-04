59fa8757b9e06ee1f83aa91d4ad248fd3220a9ee
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-11-03 22:32:47 -0300

    Editando e deletando pokémons estaticamente

--- js/time.js
+++ js/time.js
@@ -7,12 +7,12 @@ while (contador < times.length) {
     var posicaoPokemon = 0;
     var numeroPokemons = times[contador].pokemons.length; 
 
-    var time = '<div class="team">';
+    var time = `<div class="team" timeid="${times[contador].id}">`;
     time += '<div class="team-pokemons">';
 
     while (posicaoPokemon < numeroPokemons) {
         
-        time += `<div idpokemon="${times[contador].pokemons[posicaoPokemon]}" class="team-pokemon"></div>`;
+        time += `<div idpoke="${times[contador].pokemons[posicaoPokemon]}" class="team-pokemon"></div>`;
         posicaoPokemon++;
     }
 
@@ -20,8 +20,8 @@ while (contador < times.length) {
     time += '<div class="team-info">';
     time += times[contador].nome;
     time += '<div class="controls">'
-    time += '<img src="img/icons/edit.svg">';
-    time += '<img src="img/icons/delete.svg">';
+    time += '<img src="img/icons/edit.svg" onclick="editar(this)">';
+    time += '<img src="img/icons/delete.svg" onclick="deletar(this)">';
     time += '</div></div></div>';
     teams.innerHTML += time;
 
@@ -36,7 +36,7 @@ while (contador < timePokemons.length) {
     
     var pokemon = timePokemons[contador];
 
-    var id = pokemon.getAttribute('idpokemon');
+    var id = pokemon.getAttribute('idpoke');
     pokemon.style.backgroundImage = `url("img/pokemons/${fixarCasas(id)}.png")`;
 
     contador++;
@@ -96,5 +96,43 @@ function remover(e) {
 
 function novo() {
     
+    var sPokemons = selectedPokemons.childNodes;
+    
+    while (selectedPokemons.childNodes.length > 0) {
+
+        remover(sPokemons[0]);
+    }
+    
+    nome.value = '';
     teamForm.style.display = 'block';
+}
+
+function editar(e) {
+
+    // resetando o formulario de times para evitar que duplique os pokémons
+    novo();
+
+    var timeID = e.parentNode.parentNode.parentNode.getAttribute('timeid');
+    var time = times[timeID - 1];
+
+    nome.value = time.nome;
+
+    for (var i = 0; i < time.pokemons.length; i++) {
+
+        var idPokemon = time.pokemons[i];
+        var pokemon = document.querySelector(`[idpokemon='${idPokemon}']`);
+
+        var clone = pokemon.cloneNode(true);
+        clone.setAttribute('onclick', 'remover(this)');
+        pokemon.classList.add('selected');
+
+        selectedPokemons.appendChild(clone);
+    }
+
+    teamForm.style.display = 'block';
+}
+
+function deletar(e) {
+    
+    teams.removeChild(e.parentNode.parentNode.parentNode);
 }
