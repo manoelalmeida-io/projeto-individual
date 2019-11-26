e98f0c9ede06c73c6491cc8b2c7d9c78f018e073
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-11-26 00:23:13 -0300

    Busca de Pokémons por texto na Pokedéx

--- public/js/pokedex.js
+++ public/js/pokedex.js
@@ -26,6 +26,8 @@ async function load() {
         if (response.ok) {
             response.json().then(function (resposta) {
 
+                corpoTabela.innerHTML = '';
+
                 for (i = 0; i < resposta.length; i++) {
                     var registro = resposta[i];
     
@@ -43,32 +45,6 @@ async function load() {
                     row += `<td class="id"><img src="img/pokemons/${fixarCasas(id)}.png">${fixarCasas(id)}</td>`;
                     row += `<td>${nome}</td>`;
                     row += `<td name="tipo"></td>`;
-
-                    fetch(`/tipos/${id}`, { cache: 'no-store' }).then(function (response) {
-                        if (response.ok) {
-                            response.json().then(function (resposta) {
-                    
-                                var tipos = document.getElementsByName('tipo');
-
-                                for (i = 0; i < resposta.length; i++) {
-                                    var registro = resposta[i];
-
-                                    var tipo = document.createElement('span');
-                                    tipo.classList.add('type');
-                                    tipo.innerHTML = registro.nome;
-                                    tipo.style.backgroundColor = registro.cor;
-
-                                    tipos[registro.idpokemon - 1].appendChild(tipo);
-                                }
-                            });
-                        } else {
-                            console.error('Nenhum dado encontrado ou erro na API');
-                        }
-                    })
-                    .catch(function (error) {
-                        console.error(`Erro na obtenção dos dados dos tipos: ${error.message}`);
-                    });
-
                     row += `<td><b>${total}</b></td>`;
                     row += `<td>${hp}</td>`;
                     row += `<td>${atk}</td>`;
@@ -77,9 +53,104 @@ async function load() {
                     row += `<td>${spDef}</td>`;
                     row += `<td>${vel}</td>`;
                     row += `<tr>`;
-    
-                    pokemonTable.innerHTML += row;
+                    
+                    corpoTabela.innerHTML += row;
                 }
+
+                colocarTipos(resposta);
+            });
+        } else {
+            console.error('Nenhum dado encontrado ou erro na API');
+        }
+    })
+    .catch(function (error) {
+        console.error(`Erro na obtenção dos dados dos Pokémons: ${error.message}`);
+    });
+}
+
+function colocarTipos(pokemons) {
+    
+    for (let i = 0; i < pokemons.length; i++) {
+
+        let id = pokemons[i].idpokemon;
+
+        fetch(`/tipos/${id}`, { cache: 'no-store' }).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (resposta) {
+        
+                    var tipos = document.getElementsByName('tipo');
+    
+                    for (let c = 0; c < resposta.length; c++) {
+                        var registro = resposta[c];
+    
+                        var tipo = document.createElement('span');
+                        tipo.classList.add('type');
+                        tipo.innerHTML = registro.nome;
+                        tipo.style.backgroundColor = registro.cor;
+    
+                        tipos[i].appendChild(tipo);
+                    }
+                });
+            } else {
+                console.error('Nenhum dado encontrado ou erro na API');
+            }
+        })
+        .catch(function (error) {
+            console.error(`Erro na obtenção dos dados dos tipos: ${error.message}`);
+        });
+    }
+}
+
+function buscar() {
+
+    var textoPesquisa = pesquisa.value;
+    var url;
+
+    if (textoPesquisa.length > 1) {
+        
+        url = `/pokemons/buscar/${textoPesquisa}`;
+    }
+    else {
+
+        url = `/pokemons`;
+    }
+
+    fetch(url, { cache: 'no-store' }).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (resposta) {
+                
+                corpoTabela.innerHTML = '';
+
+                for (i = 0; i < resposta.length; i++) {
+                    var registro = resposta[i];
+    
+                    var id = registro.idpokemon;
+                    var nome = registro.nome;
+                    var hp = registro.hp;
+                    var atk = registro.ataque;
+                    var def = registro.defesa;
+                    var spAtk = registro.ataqueesp;
+                    var spDef = registro.defesaesp;
+                    var vel = registro.velocidade;
+                    var total = hp + atk + def + spAtk + spDef + vel; 
+    
+                    var row = '<tr>';
+                    row += `<td class="id"><img src="img/pokemons/${fixarCasas(id)}.png">${fixarCasas(id)}</td>`;
+                    row += `<td>${nome}</td>`;
+                    row += `<td name="tipo"></td>`;
+                    row += `<td><b>${total}</b></td>`;
+                    row += `<td>${hp}</td>`;
+                    row += `<td>${atk}</td>`;
+                    row += `<td>${def}</td>`;
+                    row += `<td>${spAtk}</td>`;
+                    row += `<td>${spDef}</td>`;
+                    row += `<td>${vel}</td>`;
+                    row += `<tr>`;
+                    
+                    corpoTabela.innerHTML += row;
+                }
+
+                colocarTipos(resposta);
             });
         } else {
             console.error('Nenhum dado encontrado ou erro na API');
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -3,10 +3,29 @@ var router = express.Router();
 var sequelize = require('../models').sequelize;
 var Pokemon = require('../models').Pokemon;
 
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
 /* Recuperar todos os Pokémons */
 router.get('/', function (req, res, next) {
 	console.log('Recuperando todos os Pokémons');
 	Pokemon.findAndCountAll().then(resultado => {
+		console.log(`${resultado.count} registros`);
+
+		res.json(resultado.rows);
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+	});
+});
+
+/* Recuperar todos os Pokémons com base no texto da pesquisa */
+router.get('/buscar/:texto', function (req, res, next) {
+	console.log('Recuperando todos os Pokémons com base no texto da pesquisa');
+
+	let texto = req.params.texto;
+
+	Pokemon.findAndCountAll({ where: { nome: { [Op.like]: `%${texto}%` } } }).then(resultado => {
 		console.log(`${resultado.count} registros`);
 
 		res.json(resultado.rows);
