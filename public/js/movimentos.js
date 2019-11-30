ed92cecc177a6b59bbc62754687c6b6ebd663522
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-11-30 02:36:37 -0300

    Tabela de movimentos

--- public/js/movimentos.js
+++ public/js/movimentos.js
@@ -1,35 +1,59 @@
-var movimentos = data.movimentos;
-var contador = 0;
+var movimentos = [];
+var finalizado = false;
 
-async function load() {
+function load() {
+    
+    consultaBanco();
+    atualizarDados();
+}
 
-    await fetch(`/movimentos`, { cache: 'no-store' }).then(function (response) {
+function atualizarDados() {
+    
+    if (!finalizado) {
+        
+        setTimeout(atualizarDados, 1000);
+    }
+    else {
+
+        preencherTabela(movimentos);
+    }
+}
+
+function consultaBanco() {
+
+    fetch(`/movimentos/completo`, { cache: 'no-store' }).then(function (response) {
         if (response.ok) {
             response.json().then(function (resposta) {
 
                 for (i = 0; i < resposta.length; i++) {
                     var registro = resposta[i];
 
+                    var id = registro.idmovimento;
                     var nome = registro.nome;
-                    var tipo = registro.fktipo;
-                    var categoria = registro.fkcategoria;
-                    var poder = registro.poder;
-                    var precisao = registro.precisao;
-                    var pp = registro.pp;
+                    var tipo = registro.tipo;
+                    var categoria = registro.categoria;
+                    var poder = registro.poder == null ? '-' : registro.poder;
+                    var precisao = registro.precisao == null ? '-' : registro.precisao;
+                    var pp = registro.pp == null ? '-' : registro.pp;
                     var efeito = registro.efeito;
+                    var cor = registro.cor;
 
-                    var row = '<tr>';
-                    row += `<td>${nome}</td>`;
-                    row += `<td>${tipo}</td>`;
-                    row += `<td>${categoria}</td>`;
-                    row += `<td>${poder}</td>`;
-                    row += `<td>${precisao}</td>`;
-                    row += `<td>${pp}</td>`;
-                    row += `<td>${efeito}</td>`;
-                    row += `<tr>`;
-
-                    moveTable.innerHTML += row;
+                    movimentos.push(
+                        {
+                            "idmovimento": id,
+                            "nome": nome,
+                            "tipo": tipo,
+                            "categoria": categoria,
+                            "poder": poder,
+                            "precisao": precisao,
+                            "pp": pp,
+                            "efeito": efeito,
+                            "cor": cor
+                        }
+                    );
                 }
+
+                finalizado = true;
             });
         } else {
             console.error('Nenhum dado encontrado ou erro na API');
@@ -38,6 +62,58 @@ async function load() {
         .catch(function (error) {
             console.error(`Erro na obtenção dos dados dos tipos: ${error.message}`);
         });
+}
+
+async function preencherTabela(lista) {
+    
+    corpoTabela.innerHTML = '';
+    var tablerow = '';
+
+    for (var i = 0; i < lista.length; i++) {
+
+        var movimento = lista[i];
+
+        var nome = movimento.nome;
+        var tipo = movimento.tipo;
+        var categoria = movimento.categoria;
+        var poder = movimento.poder;
+        var precisao = movimento.precisao;
+        var pp = movimento.pp;
+        var efeito = movimento.efeito;
+        var cor = movimento.cor;
+
+        var row = '<tr>';
+        row += `<td>${nome}</td>`;
+        row += `<td><span class="type" style="background-color: ${cor}">${tipo}</span></td>`;
+        row += `<td>${categoria}</td>`;
+        row += `<td>${poder}</td>`;
+        row += `<td>${precisao}</td>`;
+        row += `<td>${pp}</td>`;
+        row += `<td>${efeito}</td>`;
+        row += `<tr>`;
+        
+        tablerow += row;
+    }
+
+    corpoTabela.innerHTML = tablerow;
+}
+
+function buscar() {
+
+    var textoPesquisa = pesquisa.value.toLowerCase();
+
+    if (textoPesquisa == '') {
+        
+        preencherTabela(movimentos);
+        return;
+    }
+
+    var busca = movimentos.filter(registro => {
+
+        return registro.nome.toLowerCase().includes(textoPesquisa);
+    });
+
+    preencherTabela(busca);
 }
 
 window.onload = load();
--- routes/movimentos.js
+++ routes/movimentos.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var sequelize = require('../models').sequelize;
 var Movimento = require('../models').Movimento;
 
-/* Recuperar todos os Pokémons */
+/* Recuperar todos os movimentos */
 router.get('/', function (req, res, next) {
 
-    console.log('Recuperando todos os Pokémons');
+    console.log('Recuperando todos os movimentos');
     
 	Movimento.findAndCountAll().then(resultado => {
 
@@ -18,6 +18,33 @@ router.get('/', function (req, res, next) {
 		console.error(erro);
 		res.status(500).send(erro.message);
 	});
+});
+
+/* Recuperando os movimentos completos */
+router.get('/completo', function (req, res, next) {
+
+    console.log(`Recuperando os movimentos completos`);
+
+	const instrucaoSql = `select idmovimento, tbmovimento.nome, tbtipo.nome tipo, tbcategoria.nome categoria, 
+							poder, precisao, pp, efeito, tbtipo.cor from tbmovimento
+							inner join tbtipo on fktipo = idtipo
+                            inner join tbcategoria on fkcategoria = idcategoria
+                            order by tbmovimento.nome`;
+
+	sequelize.query(instrucaoSql, {
+        type: sequelize.QueryTypes.SELECT
+    })
+    .then(resultado => {
+
+        console.log(`Encontrados: ${resultado.length}`);
+        res.json(resultado);
+
+    }).catch(erro => {
+
+        console.error(erro);
+        res.status(500).send(erro.message);
+
+    });
 });
 
 module.exports = router;
